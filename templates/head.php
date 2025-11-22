3f424a9da1864f0dd61381db3a39150552ec111e
Kam1kot <kam1k0to@mail.ru>
2025-11-22 15:36:47 +0300

    Добавлен favicon

--- templates/head.php
+++ templates/head.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title?></title>
+    <link rel="icon" href="favicon.png" type="image/png">
     <link rel="stylesheet" href="<?= HOST ?>src/css/swiper-bundle.min.css">
     <link rel="stylesheet" href="<?= HOST ?>src/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= HOST ?>src/css/header.css">
